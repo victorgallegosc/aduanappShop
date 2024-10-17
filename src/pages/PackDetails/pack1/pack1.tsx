d6288caa304e69358e7d83a5330775294ae7a29f
victorgallegosc <victorgc11@outlook.com>
2024-10-17 00:30:02 -0600

    Create component for the service details

--- src/pages/PackDetails/pack1/pack1.tsx
+++ src/pages/PackDetails/pack1/pack1.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import styles from "./Pack1Details.module.css"
+import headerImage from "../../../assets/img/pack1.png"
+import icon from "../../../assets/icons/icon1.svg"
+
+// En este componente, aparece una linea blanca de 1px entre el Header y este componente
 
 const Pack1Details: React.FC = () => {
     return (
-        <div>
-            <p>Pack 1</p>
+        <div style={{ height: "400px", position: "relative" }}>
+            <img
+                src={headerImage}
+                style={{
+                    height: "fit-content",
+                    width: "100%",
+                    objectFit: "cover",
+                }}
+            ></img>
+            <div className={styles.iconDiv}>
+                <img src={icon} style={{ width: "150px" }}></img>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Pack1Details;
