a191e55eaaba5a82f4076689fdd7f885e09827d2
victorgallegosc <victorgc11@outlook.com>
2024-10-22 23:03:44 -0600

    Make ServicePacks Component responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Header.module.css";
 import logo from "../../assets/logo-blanco.svg";
+import shortlogo from "../../assets/short-logo-blanco.svg"
 
 const Header: React.FC = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 480);
+        };
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    
     return (
         <header className={styles.header}>
-            <img src={logo} alt="Aduanapp Logo" className={styles.logo}></img>
+            {isMobile ? (
+            <img
+                src={shortlogo}
+                alt="Aduanapp Logo"
+                className={styles.logo}
+            ></img>
+            ) :
+            <img
+                src={logo}
+                alt="Aduanapp Logo"
+                className={styles.logo}
+            ></img>}
             <div className={["font-body-bold", styles.contact].join(" ")}>
                 <p>MX</p>
                 <p>+52 56 3836 3290</p>
