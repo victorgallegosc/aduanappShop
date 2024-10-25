5d9ecb16f513ad4819eed310b3c2c41cbcd8c788
victorgallegosc <victorgc11@outlook.com>
2024-10-24 19:48:02 -0600

    Make Information Portal component responsive

--- src/components/InformationPortal/index.tsx
+++ src/components/InformationPortal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./InformationPortal.module.css"
 
@@ -35,138 +35,284 @@ const InformationPortal: React.FC = () => {
         navigate(-1);
     }
 
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 720);
+        };
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
-        <div className={styles.container}>
-            <div className={styles.containerHeader}>
-                <h2>Informacion requerida*</h2>
+        <>
+            {isMobile ? (
                 <div className={styles.containerHeaderMessage}>
                     <p className="font-body-bold">
                         ¡Ya casi, 2 pasos más y terminamos!
                     </p>
                 </div>
-            </div>
-
-            <form className={styles.formContainer} onSubmit={handleSubmit}>
-                <div className={styles.formField}>
-                    <label>Nombre*</label>
-                    <input
-                        name="nombre"
-                        type="text"
-                        placeholder="Nombre"
-                        value={formData.nombre}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className={styles.formField}>
-                    <label>Correo</label>
-                    <input
-                        name="correo"
-                        type="email"
-                        placeholder="Correo"
-                        value={formData.correo}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className={styles.formField}>
-                    <label>Apellido*</label>
-                    <input
-                        name="apellido"
-                        type="text"
-                        placeholder="Apellido"
-                        value={formData.apellido}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className={styles.formField}>
-                    <label>Teléfono*</label>
-                    <input
-                        name="telefono"
-                        type="tel"
-                        placeholder="xxx-xxx-xxxx"
-                        value={formData.telefono}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className={styles.formField}>
-                    <label>RFC*</label>
-                    <input
-                        name="rfc"
-                        type="text"
-                        placeholder="RFC"
-                        value={formData.rfc}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className={styles.formField}>
-                    <label>Tipo de aduana (opcional)</label>
-                    <input
-                        name="tipoDeAduana"
-                        type="text"
-                        placeholder="xxx-xxx-xxxx"
-                        value={formData.tipoDeAduana}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className={styles.formField}>
-                    <label>Tipo de Servicio*</label>
-                    <input
-                        name="tipoDeServicio"
-                        type="text"
-                        placeholder="Importación o Exportación"
-                        value={formData.tipoDeServicio}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className={styles.formField}>
-                    <label>País de Origen (opcional)</label>
-                    <input
-                        name="origen"
-                        type="text"
-                        placeholder="xxx-xxx-xxxx"
-                        value={formData.origen}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className={styles.formField}>
-                    <label>Tipo de Persona (opcional)</label>
-                    <input
-                        name="persona"
-                        type="text"
-                        placeholder="Moral o Física"
-                        value={formData.persona}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className={styles.formField}>
-                    <label>Código Arancelario (opcional)</label>
-                    <input
-                        name="codigo"
-                        type="text"
-                        placeholder="xxx-xxx-xxxx"
-                        value={formData.codigo}
-                        onChange={handleChange}
-                    />
-                </div>
-
-                {/* Buttons */}
-                <div className="back-and-continue-button">
-                    <div
-                        className="secondary-button font-body"
-                        onClick={goBack}
+            ) : (
+                ""
+            )}
+            <div className={styles.container}>
+                {isMobile ? (
+                    <form
+                        className={styles.formContainerMbl}
+                        onSubmit={handleSubmit}
                     >
-                        Atrás
-                    </div>
-                    <button className="primary-button font-body" type="submit">
-                        Continuar
-                    </button>
-                </div>
-            </form>
-        </div>
+                        <div className={styles.formField}>
+                            <h2>Informacion requerida*</h2>
+                            <label>Nombre*</label>
+                            <input
+                                name="nombre"
+                                type="text"
+                                placeholder="Nombre"
+                                value={formData.nombre}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className={styles.formField}>
+                            <label>Apellido*</label>
+                            <input
+                                name="apellido"
+                                type="text"
+                                placeholder="Apellido"
+                                value={formData.apellido}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className={styles.formField}>
+                            <label>Correo</label>
+                            <input
+                                name="correo"
+                                type="email"
+                                placeholder="Correo"
+                                value={formData.correo}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className={styles.formField}>
+                            <label>Teléfono*</label>
+                            <input
+                                name="telefono"
+                                type="tel"
+                                placeholder="Teléfono"
+                                value={formData.telefono}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className={styles.formField}>
+                            <label>RFC*</label>
+                            <input
+                                name="rfc"
+                                type="text"
+                                placeholder="RFC"
+                                value={formData.rfc}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className={styles.formField}>
+                            <label>Tipo de Servicio*</label>
+                            <input
+                                name="tipoDeServicio"
+                                type="text"
+                                placeholder="Importación o Exportación"
+                                value={formData.tipoDeServicio}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className={styles.formField}>
+                            <label>Tipo de Persona (opcional)</label>
+                            <input
+                                name="persona"
+                                type="text"
+                                placeholder="Moral o Física"
+                                value={formData.persona}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className={styles.formField}>
+                            <label>Tipo de aduana (opcional)</label>
+                            <input
+                                name="tipoDeAduana"
+                                type="text"
+                                placeholder="Tipo de Aduana"
+                                value={formData.tipoDeAduana}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className={styles.formField}>
+                            <label>País de Origen (opcional)</label>
+                            <input
+                                name="origen"
+                                type="text"
+                                placeholder="País de Origen"
+                                value={formData.origen}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className={styles.formField}>
+                            <label>Código Arancelario (opcional)</label>
+                            <input
+                                name="codigo"
+                                type="text"
+                                placeholder="Código Arancelario"
+                                value={formData.codigo}
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        {/* Buttons */}
+                        <div className="back-and-continue-button">
+                            <div
+                                className="secondary-button font-body"
+                                onClick={goBack}
+                            >
+                                Atrás
+                            </div>
+                            <button
+                                className="primary-button font-body"
+                                type="submit"
+                            >
+                                Continuar
+                            </button>
+                        </div>
+                    </form>
+                ) : (
+                    <>
+                        <div className={styles.containerHeader}>
+                            <h2>Informacion requerida*</h2>
+                            <div className={styles.containerHeaderMessage}>
+                                <p className="font-body-bold">
+                                    ¡Ya casi, 2 pasos más y terminamos!
+                                </p>
+                            </div>
+                        </div>
+                        <form
+                            className={styles.formContainer}
+                            onSubmit={handleSubmit}
+                        >
+                            <div className={styles.formField}>
+                                <h2>Informacion requerida*</h2>
+                                <label>Nombre*</label>
+                                <input
+                                    name="nombre"
+                                    type="text"
+                                    placeholder="Nombre"
+                                    value={formData.nombre}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className={styles.formField}>
+                                <label>Apellido*</label>
+                                <input
+                                    name="apellido"
+                                    type="text"
+                                    placeholder="Apellido"
+                                    value={formData.apellido}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className={styles.formField}>
+                                <label>Correo</label>
+                                <input
+                                    name="correo"
+                                    type="email"
+                                    placeholder="Correo"
+                                    value={formData.correo}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className={styles.formField}>
+                                <label>Teléfono*</label>
+                                <input
+                                    name="telefono"
+                                    type="tel"
+                                    placeholder="Teléfono"
+                                    value={formData.telefono}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className={styles.formField}>
+                                <label>RFC*</label>
+                                <input
+                                    name="rfc"
+                                    type="text"
+                                    placeholder="RFC"
+                                    value={formData.rfc}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className={styles.formField}>
+                                <label>Tipo de Servicio*</label>
+                                <input
+                                    name="tipoDeServicio"
+                                    type="text"
+                                    placeholder="Importación o Exportación"
+                                    value={formData.tipoDeServicio}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className={styles.formField}>
+                                <label>Tipo de Persona (opcional)</label>
+                                <input
+                                    name="persona"
+                                    type="text"
+                                    placeholder="Moral o Física"
+                                    value={formData.persona}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className={styles.formField}>
+                                <label>Tipo de aduana (opcional)</label>
+                                <input
+                                    name="tipoDeAduana"
+                                    type="text"
+                                    placeholder="Tipo de Aduana"
+                                    value={formData.tipoDeAduana}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className={styles.formField}>
+                                <label>País de Origen (opcional)</label>
+                                <input
+                                    name="origen"
+                                    type="text"
+                                    placeholder="País de Origen"
+                                    value={formData.origen}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className={styles.formField}>
+                                <label>Código Arancelario (opcional)</label>
+                                <input
+                                    name="codigo"
+                                    type="text"
+                                    placeholder="Código Arancelario"
+                                    value={formData.codigo}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </form>
+                    </>
+                )}
+            </div>
+        </>
     );
 }
 
